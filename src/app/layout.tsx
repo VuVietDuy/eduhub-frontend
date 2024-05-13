5ecdfe7c30630b334ffa309898edba3c86ad7f3a
unknown <lamtx.3107@gmail.com>
2024-05-13 13:50:28 +0700

    setup redux

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type {Metadata} from 'next';
 import './globals.css';
 import {Providers} from '../components/Providers';
+import {Provider} from 'react-redux';
+import {store} from '@/store/store';
 
 export const metadata: Metadata = {
   title: 'EduHub',
@@ -12,7 +14,9 @@ export default function RootLayout({children}: {children: React.ReactNode}) {
   return (
     <html lang="en" suppressHydrationWarning>
       <body>
-        <Providers>{children}</Providers>
+        <Providers>
+          <Provider store={store}>{children}</Provider>
+        </Providers>
       </body>
     </html>
   );
