e288a7494af8a2fc9e36dab9b621f1a77d10cefb
VuVietDuy <vuvietduy1234@gmail.com>
2024-05-24 20:57:32 +0700

    Update

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -44,7 +44,7 @@ export default function Modal(props: IProps) {
             } `}
           >
             <div className="relative bg-white rounded-sm shadow dark:bg-gray-700">
-              <div className="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
+              <div className="flex items-center justify-between p-2 md:p-3 border-b rounded-t dark:border-gray-600">
                 <h3 className="text-xl font-semibold text-gray-900 dark:text-white text-center">
                   {title}
                 </h3>
@@ -63,21 +63,23 @@ export default function Modal(props: IProps) {
                 className={`p-4 md:p-5 space-y-4  max-h-[calc(100vh-100px)] overflow-y-auto`}
               >
                 {children}
-                <div className="flex items-center p-4 md:p-5 border-t border-gray-200 rounded-b dark:border-gray-600">
+              </div>
+              <div>
+                <div className="flex items-center justify-end p-2 md:p-3 border-t border-gray-200 rounded-b dark:border-gray-600">
+                  <button
+                    onClick={onCancel}
+                    type="button"
+                    className="py-2.5 px-5 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
+                  >
+                    Cancel
+                  </button>
                   <button
                     onClick={onCancel}
                     data-modal-hide="default-modal"
                     type="button"
-                    className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                    className="text-white  ms-3 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
                   >
                     Ok
-                  </button>
-                  <button
-                    onClick={onCancel}
-                    type="button"
-                    className="py-2.5 px-5 ms-3 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
-                  >
-                    Cancel
                   </button>
                 </div>
               </div>
